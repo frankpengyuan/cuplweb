321967074129a3e65cdf5848b933421f4b119041
Frank Yuan <frankpengyuan@gmail.com>
2017-09-21 20:57:15 -0700

    czx

--- cuplweb/robincat/views.py
+++ cuplweb/robincat/views.py
@@ -12,7 +12,7 @@ def get_price(request):
     stock_info = json.loads(urllib.request.urlopen(
         "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&"
         "symbol={}&interval=1min&apikey=AHY5RTTYJ34OILWU".format(symbol)).read().decode('utf-8'))
-   	print(stock_info)
+    print(stock_info)
     latest_info = sorted([(time, values) 
         for time, values in stock_info.get("Time Series (1min)").items()])[-1]
     price = Decimal(latest_info[1].get("4. close"))
