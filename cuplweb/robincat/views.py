dbbee01391bdb1a6e4b5252ffbbb24e5aa286489
Frank Yuan <frankpengyuan@gmail.com>
2017-09-21 20:56:31 -0700

    czx

--- cuplweb/robincat/views.py
+++ cuplweb/robincat/views.py
@@ -8,11 +8,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def get_price(request):
-    print(request.body)
     symbol = json.loads(request.body.decode('utf-8')).get("result").get("parameters").get("symbol")
     stock_info = json.loads(urllib.request.urlopen(
         "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&"
         "symbol={}&interval=1min&apikey=AHY5RTTYJ34OILWU".format(symbol)).read().decode('utf-8'))
+   	print(stock_info)
     latest_info = sorted([(time, values) 
         for time, values in stock_info.get("Time Series (1min)").items()])[-1]
     price = Decimal(latest_info[1].get("4. close"))
